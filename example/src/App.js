ecac66eab8932d14a9e5d12bb2dac8fe735771ab
ignatif <ignatif@gmail.com>
2021-01-27 17:22:37 +0200

    basic airtable crud

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,49 @@
 import React from 'react'
-import { useMyHook } from 'use-airtable'
+import { useAirtable } from 'use-airtable'
+
+const AIRTABLE_API_KEY = 'keyl0A56Sw6XVQcY9'
+const AIRTABLE_BASE_ID = 'appMulAtpemumqDLV'
 
 const App = () => {
-  const example = useMyHook()
+  const [records, createRecord, updateRecord, deleteRecord] = useAirtable(
+    'Tasks',
+    AIRTABLE_API_KEY,
+    AIRTABLE_BASE_ID
+  )
+
   return (
     <div>
-      {example}
+      <div>
+        {records.map((record) => (
+          <div>
+            {`${record.fields.Status}: ${record.fields.Name}`}
+            <button
+              onClick={() => updateRecord(record.id, { Status: 'In progress' })}
+              style={{ marginLeft: 8, color: '#435090' }}
+            >
+              update
+            </button>
+            <button
+              onClick={() => deleteRecord(record.id)}
+              style={{ marginLeft: 8, color: '#d43636' }}
+            >
+              delete
+            </button>
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={() =>
+          createRecord({
+            Name: 'Name',
+            Status: 'Todo'
+          })
+        }
+      >
+        Create record
+      </button>
     </div>
   )
 }
+
 export default App
--- src/index.js
+++ src/index.js
@@ -1,21 +1,81 @@
-import * as React from 'react'
+import { useState, useEffect } from 'react'
 
-export const useMyHook = () => {
-  let [{
-    counter
-  }, setState] = React.useState({
-    counter: 0
-  })
+const Airtable = require('airtable')
 
-  React.useEffect(() => {
-    let interval = window.setInterval(() => {
-      counter++
-      setState({counter})
-    }, 1000)
-    return () => {
-      window.clearInterval(interval)
-    }
+export const useAirtable = (tableName, apiKey, baseId) => {
+  const [records, setRecords] = useState([])
+  const base = new Airtable({ apiKey }).base(baseId)
+
+  const getRecords = () => {
+    base('Tasks')
+      .select({
+        // Selecting the first 3 records in Grid view:
+        // maxRecords: 3,
+        view: 'Grid view'
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          setRecords(records)
+          records.forEach(function (record) {
+            console.log('Retrieved', record.get('Name'))
+          })
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage()
+        },
+        function done(err) {
+          if (err) {
+            console.error(err)
+          }
+        }
+      )
+  }
+
+  useEffect(() => {
+    getRecords()
   }, [])
 
-  return counter
+  const createRecord = (fields) =>
+    base(tableName).create(fields, (err, record) => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(record)
+    })
+
+  const updateRecord = (id, fields) => {
+    base('Tasks').update(
+      [
+        {
+          id,
+          fields
+        }
+      ],
+      function (err, records) {
+        if (err) {
+          console.error(err)
+          return
+        }
+        records.forEach(function (record) {
+          console.log(record.get('Status'))
+        })
+      }
+    )
+  }
+
+  const deleteRecord = (recordId) =>
+    base('Tasks').destroy(recordId, function (err, deletedRecord) {
+      if (err) {
+        console.error(err)
+        return
+      }
+      console.log(deletedRecord)
+    })
+
+  return [records, createRecord, updateRecord, deleteRecord]
 }

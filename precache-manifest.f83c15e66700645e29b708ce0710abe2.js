50599353a2f8a1b3876c1cdca84050419fc2d344
ignatif <ignatif@gmail.com>
2021-01-27 19:10:22 +0200

    Updates

---
+++ precache-manifest.f83c15e66700645e29b708ce0710abe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0e8cbbd42861bd0fda8169dcd0c74d",
+    "url": "/use-airtable/index.html"
+  },
+  {
+    "revision": "45b7bca498312a424c5a",
+    "url": "/use-airtable/static/css/main.2c6a48fe.chunk.css"
+  },
+  {
+    "revision": "f851ae63dd651d053e73",
+    "url": "/use-airtable/static/js/2.cc5ba809.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/use-airtable/static/js/2.cc5ba809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b7bca498312a424c5a",
+    "url": "/use-airtable/static/js/main.196efea1.chunk.js"
+  },
+  {
+    "revision": "ca62c14ece744b256d8a",
+    "url": "/use-airtable/static/js/runtime-main.00ce6e52.js"
+  }
+]);
